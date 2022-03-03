e62027a68ff05e3aeaaa4a0c4b565cd2673d9836
bilalkirkgul <61188245+bilalkirkgul@users.noreply.github.com>
2022-03-04 00:03:53 +0300

    Create Writer Profile

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -1,10 +1,12 @@
 ﻿using BLL.Abstract;
+using BLL.ValidationRules;
 using CoreDemo.Models;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -29,18 +31,50 @@ namespace CoreDemo.Controllers
             var dataValues = writerService.GetById(value);
             return View(dataValues);
         }
+
+        [HttpGet]
+        public IActionResult EditProfile()
+        {
+            //int value = int.Parse(User.FindFirstValue(ClaimTypes.UserData));
+            var dataValues = writerService.GetById(3);
+            return View(dataValues);
+        }
+        [HttpPost]
+        public IActionResult WriterEditProfile(Writer writer)
+        {
+            WriterValidator wl = new WriterValidator();
+            FluentValidation.Results.ValidationResult result = wl.Validate(writer);
+
+            if (result.IsValid)
+            {
+                writer.WriterStatus = true;
+                writerService.Update(writer);
+                return RedirectToAction("Index", "Dashboard");
+            }
+            else
+            {
+                //Tüm hataları dön propertysine hata mesajını ekle
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+            return View();
+        }
+
         public IActionResult WriterSlidebar()
         {
             int value = int.Parse(User.FindFirstValue(ClaimTypes.UserData));
             var dataValues = writerService.GetById(value);
-            return PartialView();
+            return PartialView("WriterSlidebar",dataValues);
         }
 
         public IActionResult WriterNavbar()
         {
             int value = int.Parse(User.FindFirstValue(ClaimTypes.UserData));
             var dataValues = writerService.GetById(value);
-            return PartialView();
+             return PartialView("WriterNavbar",dataValues);
         }
 
 
